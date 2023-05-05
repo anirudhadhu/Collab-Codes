658b03bf7963a13695800c2fd000670de1290bc4
anirudhadhu <anirudradhungana@gmail.com>
2023-05-05 19:43:24 +0545

    minor changes in css

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -22,18 +22,18 @@ const LoginForm = () => {
 
   return (
     <div> 
-      <h1>Tax Calculation system</h1>
+      <h2>Tax Calculation system</h2>
       <div
       
       style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-        <h1>LogIn</h1>
+        <h3>LogIn</h3>
         <div style={{ border: '5px solid black', borderRadius: 10, padding: '30px 50px', marginBottom: '20px' }}>
           <form onSubmit={handleSubmit}>
             <div style={{ marginBottom: '20px' }}>
               <input
                 type="text"
                 name="username"
-                style={{ border: '2px solid #e8e8e8', padding: 15, borderRadius: 10, backgroundColor: '#212121', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: 'white' }}
+                style={{ border: '2px solid black', padding: 15, borderRadius: 10, backgroundColor: 'white', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: 'black' }}
                 className="input"
                 placeholder="Enter your username"
                 value={username}
@@ -44,14 +44,14 @@ const LoginForm = () => {
               <input
                 type="password"
                 name="password"
-                style={{ border: '2px solid #e8e8e8', padding: 15, borderRadius: 10, backgroundColor: '#212121', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: '#e8e8e8' }}
+                style={{ border: '2px solid black', padding: 15, borderRadius: 10, backgroundColor: 'white', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: 'black' }}
                 className="input"
                 placeholder="Enter your password"
                 value={password}
                 onChange={handlePasswordChange}
               />
             </div>
-            <button className="submit" type="submit" onClick={handleSubmit}  style={{ display: 'block', margin: '0 auto', border: 'none', backgroundColor: '#3d405b', color: '#FFFFFF', textAlign: 'center', fontSize: '17px', padding: '16px', width: '130px', borderRadius: '4px', cursor: 'pointer' }}>
+            <button className="submit" type="submit" onClick={handleSubmit}   style={{ display: 'block', margin: '0 auto', border: 'black', backgroundColor: 'none', color: 'black', textAlign: 'center', fontSize: '17px', padding: '16px', width: '130px', borderRadius: '4px', cursor: 'pointer' }}>
               Login
             </button>
           </form>
--- src/SignupPage.js
+++ src/SignupPage.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
+import logo from "./images/1logo.png";  
+
+
 
 const SignupPage = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [Username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
 
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
-  };
 
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
   };
 
   const handleEmailChange = (event) => {
@@ -23,46 +21,40 @@ const SignupPage = () => {
     setPassword(event.target.value);
   };
 
-  const handleConfirmPasswordChange = (event) => {
-    setConfirmPassword(event.target.value);
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`Submitting signup form with first name: ${firstName}, last name: ${lastName}, email: ${email}, password: ${password}, confirm password: ${confirmPassword}`);
+    console.log(`Submitting signup form with Username: ${Username}, email: ${email}, password: ${password}`);
   };
 
   return (
+    <div> 
+      <h2>Tax Calculation system</h2>
+    
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <h3> Sign Up</h3>
+      <h4> Hello there! Signin and start calculating your taxes</h4>
       <div style={{ border: '5px solid black', borderRadius: 10, padding: '30px 50px', marginBottom: '20px' }}>
         <form onSubmit={handleSubmit}>
           <div style={{ marginBottom: '20px' }}>
             <input
               type="text"
-              name="firstName"
-              style={{ border: '2px solid #e8e8e8', padding: 15, borderRadius: 10, backgroundColor: '#212121', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: 'white' }}
+              name="Username"
+              style={{ border: '2px solid black', padding: 15, borderRadius: 10, backgroundColor: 'white', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: 'black' }}
               className="input"
-              placeholder="Enter your first name"
-              value={firstName}
-              onChange={handleFirstNameChange}
+              placeholder="Enter username"
+              value={Username}
+              onChange={handleUsernameChange}
             />
           </div>
-          <div style={{ marginBottom: '20px' }}>
-            <input
-              type="text"
-              name="lastName"
-              style={{ border: '2px solid #e8e8e8', padding: 15, borderRadius: 10, backgroundColor: '#212121', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: 'white' }}
-              className="input"
-              placeholder="Enter your last name"
-              value={lastName}
-              onChange={handleLastNameChange}
-            />
+          <div style={{position: 'fixed', width: '100px', height: '100px', left: '100px', top: '0px'}}> 
+
           </div>
+          
           <div style={{ marginBottom: '20px' }}>
             <input
               type="email"
               name="email"
-              style={{ border: '2px solid #e8e8e8', padding: 15, borderRadius: 10, backgroundColor: '#212121', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: 'white' }}
+              style={{ border: '2px solid #black', padding: 15, borderRadius: 10, backgroundColor: 'white', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: 'black' }}
               className="input"
               placeholder="Enter your email address"
               value={email}
@@ -73,7 +65,7 @@ const SignupPage = () => {
           <input
                type="password"
                name="password"
-               style={{ border: '2px solid #e8e8e8', padding: 15, borderRadius: 10, backgroundColor: '#212121', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: '#e8e8e8' }}
+               style={{ border: '2px solid #black', padding: 15, borderRadius: 10, backgroundColor: 'white', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: 'black' }}
                 className="input"
                 placeholder="Enter your password"
                 value={password}
@@ -81,28 +73,19 @@ const SignupPage = () => {
 />
 
               </div>
-              <div style={{ marginBottom: '20px' }}>
-          <input
-               type="password"
-               name="password"
-               style={{ border: '2px solid #e8e8e8', padding: 15, borderRadius: 10, backgroundColor: '#212121', fontSize: 'small', fontWeight: 'bold', textAlign: 'center', color: '#e8e8e8' }}
-                className="input"
-                placeholder="Confirm your password "
-                value={confirmPassword}
-                onChange={handleConfirmPasswordChange}
-/>
-
-              </div>
-              <button className="submit" type="submit" onClick={handleSubmit}  style={{ display: 'block', margin: '0 auto', border: 'none', backgroundColor: '#3d405b', color: '#FFFFFF', textAlign: 'center', fontSize: '17px', padding: '16px', width: '130px', borderRadius: '4px', cursor: 'pointer' }}>
+    
+              <button className="submit" type="submit" onClick={handleSubmit}  style={{ display: 'block', margin: '0 auto', border: 'black', backgroundColor: 'none', color: 'black', textAlign: 'center', fontSize: '17px', padding: '16px', width: '130px', borderRadius: '4px', cursor: 'pointer' }}>
               Signup
             </button>
               </form>
-
               </div>
               </div>
+              </div>
+              
   
     
   );
 }
 
 export default SignupPage;
+
